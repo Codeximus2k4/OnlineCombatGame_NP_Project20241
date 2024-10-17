0afb4146bd8aad6e7110939dd4268b58741d4a4a
nhhsag12 <nhhoang12122004@gmail.com>
2024-10-17 23:52:32 +0700

    Fix attack animation

--- scripts/entities.py
+++ scripts/entities.py
@@ -26,22 +26,55 @@ class Player(PhysicsEntity):
     def __init__(self, id, game,pos,size):
         super().__init__(game, 'player', pos, size)
         self.id = id
-        self.air_time = 0
+        self.dashing = 0
+        self.jump_cooldown = 3
         self.speed = 3
         self.dashing = 0
-    def update(self, movement = [0,0]):
-        if movement[0]>0:
-            self.flip = False
-            self.set_action('run')
-        elif movement[0]<0:
-            self.flip = True
-            self.set_action('run')
-        else:
-            self.set_action('idle')
+        self.isAttacking = False
+        self.attack_cooldown = 5
+        self.timeSinceAttack = 0
         
-        self.velocity[0]= self.speed * movement[0]
-        frame_movement=  (self.velocity[0],self.velocity[1])
-        self.pos[0]+= frame_movement[0]
+    
+    def update(self, movement = [0,0]):
+
+        #If not in the animation of attacking then the character can move
+        if not self.isAttacking :
+            if movement[0]>0:
+                self.flip = False
+                self.set_action('run')
+            elif movement[0]<0:
+                self.flip = True
+                self.set_action('run')
+            else:
+                self.set_action('idle')
+        
+            self.velocity[0]= self.speed * movement[0]
+            frame_movement=  (self.velocity[0],self.velocity[1])
+            self.pos[0]+= frame_movement[0]
+
+            # update on whether the player can attack or not with the cooldown time
+            self.timeSinceAttack = min (self.timeSinceAttack+1, self.attack_cooldown+1)
+        else:
+            if self.animation.done:
+                self.isAttacking = False
+                self.set_action('idle')
         self.animation.update()
+
+
     def render(self,surf):
         super().render(surf)
+
+    def ground_attack(self, attack_animation,attack_cooldown = 3):
+        if self.timeSinceAttack > self.attack_cooldown:
+            self.attack_cooldown = attack_cooldown
+            self.isAttacking = True
+            self.set_action(attack_animation)
+            self.timeSinceAttack = 0
+    def serialize_data(self):
+        return "player|" + str(self.id) +"|"+str(self.pos[0])+"|"+str(self.pos[1])+"|"+str(int(self.flip))+"|"+self.action+"|"+str(self.animation.frame)
+
+    
+    def render(self,surf):
+        super().render(surf)
+    
+    
