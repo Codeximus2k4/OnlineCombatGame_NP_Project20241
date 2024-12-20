8b206984fd07dd76d04fa4faa05eb0c66f729122
Nguyễn Hữu Nam Hòa <132972072+Codeximus2k4@users.noreply.github.com>
2024-12-20 01:32:23 +0000

    Update entities.py

--- scripts/entities.py
+++ scripts/entities.py
@@ -4,77 +4,81 @@ import pygame
 BASE_IMG_PATH =  'data/images'
 
 class PhysicsEntity:
-    def __init__(self, game, entity_type, pos, size):
+    def __init__(self, game, id, entity_type, entity_class,  pos,flip):
         self.game =  game
-        self.entity_type =  entity_type
-        self.pos = list(pos)
-        self.size = size
-        self.velocity = [0,0]
-        self.flip =  False
-        self.action = ''
-        self.set_action('idle')
-    def set_action(self,action):
-        if action!= self.action:
-            self.action  = action
-            self.animation =  self.game.assets[self.entity_type+'/'+self.action].copy()
-    def render(self,surf):
-        if self.flip:
-            surf.blit(pygame.transform.flip(self.animation.get_img(),self.flip, False),dest = self.pos)
-        else:
-            surf.blit(self.animation.get_img(),dest = self.pos)
-class Player(PhysicsEntity):
-    def __init__(self, id, game,pos,size):
-        super().__init__(game, 'player', pos, size)
-        self.id = id
-        self.dashing = 0
-        self.jump_cooldown = 3
-        self.speed = 3
-        self.dashing = 0
-        self.isAttacking = False
-        self.attack_cooldown = 5
-        self.timeSinceAttack = 0
-        
+        self.id =  id
+        self.entity_type= entity_type
+        self.entity_class = entity_class
+        self.pos = list(pos) 
+        self.flip=flip
+
+    def convert_entity_class(self):
+        raise NotImplementedError("Have not implemented this method")
+    def convert_action_type(self):
+        raise NotImplementedError("Have not implemented this method")
     
-    def update(self, movement = [0,0]):
-
+    def set_action(self,action,fresh_start):
+        if action!= self.action_type or fresh_start:
+            fresh_start=False
+            self.action_type= action
+            self.animation =  self.game.assets[self.convert_entity_class()+'/'+self.convert_action_type()].copy()
+    
+    def render(self,surf):
+            surf.blit(pygame.transform.flip(self.animation.get_img(),self.flip, False),dest = self.pos)
+class Player(PhysicsEntity):
+    def __init__(self, game, id, entity_class, posx, posy,health, stamina ):
+        super().__init__(game, id, 0, entity_class, (posx, posy),flip=False)
+        self.health =  health
+        self.stamina = stamina
+        self.action_type  = 0
+    
+    def update(self):
+        #self.set_action(self.action_type)
         #If not in the animation of attacking then the character can move
-        if not self.isAttacking :
-            if movement[0]>0:
-                self.flip = False
-                self.set_action('run')
-            elif movement[0]<0:
-                self.flip = True
-                self.set_action('run')
-            else:
-                self.set_action('idle')
+        # if not self.isAttacking :
+        #     if movement[0]>0:
+        #         self.flip = False
+        #         self.set_action('run')
+        #     elif movement[0]<0:
+        #         self.flip = True
+        #         self.set_action('run')
+        #     else:
+        #         self.set_action('idle')
         
-            self.velocity[0]= self.speed * movement[0]
-            frame_movement=  (self.velocity[0],self.velocity[1])
-            self.pos[0]+= frame_movement[0]
+        #     self.velocity[0]= self.speed * movement[0]
+        #     frame_movement=  (self.velocity[0],self.velocity[1])
+        #     self.pos[0]+= frame_movement[0]
 
-            # update on whether the player can attack or not with the cooldown time
-            self.timeSinceAttack = min (self.timeSinceAttack+1, self.attack_cooldown+1)
-        else:
-            if self.animation.done:
-                self.isAttacking = False
-                self.set_action('idle')
+        #     # update on whether the player can attack or not with the cooldown time
+        #     self.timeSinceAttack = min (self.timeSinceAttack+1, self.attack_cooldown+1)
+        # else:
+        
+        if (self.action_type ==1 or self.action_type ==2) and self.animation.done:
+            self.set_action(0,False)
         self.animation.update()
 
-
+    def convert_entity_class(self):
+        ent_class = {0:"Samurai",1:"Evil Mage"}
+        return ent_class[self.entity_class]
+        
+    def convert_action_type(self):
+        action = {0:"idle",1:"attack1",2:"attack2",3:"run",4:"jump",5:"crouch",6:"fall",
+                  7:"special ability",8:"block",9:"dash",10:"take hit",11:"death"}
+        return action[self.action_type]
+    def set_action(self, action, fresh_start):
+        super().set_action(action, fresh_start)
     def render(self,surf):
         super().render(surf)
 
-    def ground_attack(self, attack_animation,attack_cooldown = 3):
-        if self.timeSinceAttack > self.attack_cooldown:
-            self.attack_cooldown = attack_cooldown
-            self.isAttacking = True
-            self.set_action(attack_animation)
-            self.timeSinceAttack = 0
-    def serialize_data(self):
-        return "player|" + str(self.id) +"|"+str(self.pos[0])+"|"+str(self.pos[1])+"|"+str(int(self.flip))+"|"+self.action+"|"+str(self.animation.frame)
+    # def ground_attack(self, attack_animation,attack_cooldown = 3):
+    #     if self.timeSinceAttack > self.attack_cooldown:
+    #         self.attack_cooldown = attack_cooldown
+    #         self.isAttacking = True
+    #         self.set_action(attack_animation)
+    #         self.timeSinceAttack = 0
+    # def serialize_data(self):
+    #     return "player|" + str(self.id) +"|"+str(self.pos[0])+"|"+str(self.pos[1])+"|"+str(int(self.flip))+"|"+self.action+"|"+str(self.animation.frame)
 
     
-    def render(self,surf):
-        super().render(surf)
     
     
